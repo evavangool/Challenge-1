15af8ceb395807190f6ac28f2a45e5556e2a06ca
Eva van Gool <evavangool@hotmail.com>
2019-02-18 16:34:55 +0100

    Hover toegevoegd en extra opmaak

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,7 @@
 /*DIT IS VOOR MAIN 1--------------------------------*/
 
 window.onload = function() {
-	CanvasJS.addColorSet("pieShades",
+	CanvasJS.addColorSet("pieKleuren",
 	        [//colorSet Array
 
 	        "#DE625E", //beetje roze
@@ -13,8 +13,9 @@ window.onload = function() {
 
 var eetPercentage = [87.5,75,75,75,87.5]
 
-var pie = new CanvasJS.Chart("chartContainer1", {
-		colorSet: "pieShades",
+var pie = new CanvasJS.Chart("pieEten", {
+		backgroundColor: "#E0E0E0",
+		colorSet: "pieKleuren",
 		animationEnabled: true,
 		data: [{
 		type: "pie",
@@ -36,7 +37,7 @@ pie.render();
 
 var drinkPercentage = [72, 63, 50, 91]
 
-CanvasJS.addColorSet("pieShades2",
+CanvasJS.addColorSet("pieKleuren2",
 				[//colorSet Array
 
 				"#A2D3EA", //licht blauw
@@ -45,21 +46,22 @@ CanvasJS.addColorSet("pieShades2",
 				"#E7C733", //geel
 		]);
 
-var pieTwee = new CanvasJS.Chart("chartContainer2", {
-	colorSet: "pieShades2",
-	animationEnabled: true,
-	data: [{
-	type: "pie",
-	startAngle: 240,
-	yValueFormatString: "##0.00\"%\"",
-	indexLabel: "{label} {y}",
-	dataPoints: [
-		{y: 100 - drinkPercentage[0], label: "Alcoholische dranken"},
-		{y: 100 - drinkPercentage[1] , label: "Warme dranken"},
-		{y: 100 - drinkPercentage[2], label: "Koude dranken"},
-		{y: 100 - drinkPercentage[3], label: "Sapjes"},
-	]
-}]
+var pieTwee = new CanvasJS.Chart("pieDrinken", {
+		backgroundColor: "#E0E0E0",
+		colorSet: "pieKleuren2",
+		animationEnabled: true,
+		data: [{
+		type: "pie",
+		startAngle: 240,
+		yValueFormatString: "##0.00\"%\"",
+		indexLabel: "{label} {y}",
+		dataPoints: [
+			{y: 100 - drinkPercentage[0], label: "Alcoholische dranken"},
+			{y: 100 - drinkPercentage[1] , label: "Warme dranken"},
+			{y: 100 - drinkPercentage[2], label: "Koude dranken"},
+			{y: 100 - drinkPercentage[3], label: "Sapjes"},
+		]
+	}]
 });
 pieTwee.render();
 
@@ -74,14 +76,15 @@ CanvasJS.addColorSet("donutShades",
 				"#DE625E", //beetje roze
 		]);
 
-var donut = new CanvasJS.Chart("chartContainer3", {
-	colorSet: "donutShades",
-	animationEnabled: true,
-	data: [{
+var donut = new CanvasJS.Chart("donutBrandstof", {
+		backgroundColor: "#E0E0E0",
+		colorSet: "donutShades",
+		animationEnabled: true,
+		data: [{
 		type: "doughnut",
 		startAngle: 60,
 		//innerRadius: 60,
-		indexLabelFontSize: 17,
+		indexLabelFontSize: 12,
 		indexLabel: "{label} - #percent%",
 		toolTipContent: "<b>{label}:</b> {y} (#percent%)",
 		dataPoints: [
@@ -95,18 +98,18 @@ donut.render();
 
 /*DIT IS VOOR MAIN 4--------------------------------*/
 
-var lijnTabel = new CanvasJS.Chart("chartContainer4",{
-    backgroundColor: "#FFFFFF",
+var lijnTabel = new CanvasJS.Chart("lijnGrafiekKilometer",{
+    backgroundColor: "#E0E0E0",
     animationEnabled: true,
     data: [{
     type: "line",
     lineColor: "#3E484B",
         dataPoints : [
 		    { label: "12:00 uur",  y: 1000 , markerColor: "#A2D3EA"},
-	      { label: "13:00 uur", y: 1497 , markerColor: "#67B6AA"},
+	      { label: "13:00 uur",  y: 1497 , markerColor: "#67B6AA"},
 	      { label: "14:00 uur",  y: 2060 , markerColor: "#A2D3EA"},
 	      { label: "15:00 uur",  y: 2780 , markerColor: "#67B6AA"},
-		    { label: "16:00 uur", y: 3120 , markerColor: "#A2D3EA"},
+		    { label: "16:00 uur",  y: 3120 , markerColor: "#A2D3EA"},
 		    { label: "17:00 uur",  y: 3987 , markerColor: "#67B6AA"},
 	      { label: "18:00 uur",  y: 4230 , markerColor: "#A2D3EA"},
 	      { label: "19:00 uur",  y: 4780 , markerColor: "#67B6AA"},
